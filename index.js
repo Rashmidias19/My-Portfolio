7f341da52d9eef670aff13679d1a677cfb7b53a5
Rashmidias19 <rashmidias819@gmail.com>
2023-09-15 18:35:54 +0530

    new changes done

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ function collapse(){
     var para=document.getElementById("para");
     para.classList.toggle("");
 }
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
